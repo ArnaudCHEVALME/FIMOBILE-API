53117a363967c1773a9fb2375a945260c7be572c
Nono <arnaud.851216@gmail.com>
2022-10-20 22:22:31 +0200

    added basic user controller and routes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING,
             allowNull: true
         },
-        pseudo: {
+        login: {
             type: Sequelize.STRING,
             allowNull: false
         },
