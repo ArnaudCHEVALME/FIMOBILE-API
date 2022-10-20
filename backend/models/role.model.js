88aa21fe78f576508c35a411d36df91c88ce8ad1
Nono <arnaud.851216@gmail.com>
2022-10-20 09:47:28 +0200

    added role and permission, updated users

---
+++ backend/models/role.model.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sequelize) => {
+    const Role = sequelize.define("role", {
+        roleId: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        libelle: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return Role;
+};
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,14 +1,4 @@
 const bcrypt = require('bcrypt');
-// function cryptPassword(password, callback) {
-//     bcrypt.genSalt(10, function (err, salt) { // Encrypt password using bycrpt module
-//         if (err)
-//             return callback(err);
-
-//         bcrypt.hash(password, salt, function (err, hash) {
-//             return callback(err, hash);
-//         });
-//     });
-// }
 
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("user", {
@@ -45,28 +35,8 @@ module.exports = (sequelize, Sequelize) => {
                         user.password = bcrypt.hashSync(user.password, salt);
                     }
                 }
-            },
-            instanceMethods: {
-                validPassword: (password) => {
-                    return bcrypt.compareSync(password, this.password);
-                }
             }
         }
     );
-
-    // User.beforeCreate(function (model, options, cb) {
-    //     debug('Info: ' + 'Storing the password');
-    //     cryptPassword(user.password, function (err, hash) {
-    //         if (err) return cb(err);
-    //         debug('Info: ' + 'getting ' + hash);
-
-    //         user.password = hash;
-    //         return cb(null, options);
-    //     });
-    // });
-
-    User.prototype.validPassword = async (password, hash) => {
-        return await bcrypt.compareSync(password, hash);
-    }
     return User;
 }
