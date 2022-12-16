40495facd24af748bfb441943d29bc3f9e4f1b1d
Arnaud Chevalme <arnaud.chevalme@proton.me>
2022-12-16 09:00:03 +0100

    removed association genre/artistes

--- backend/models/index.js
+++ backend/models/index.js
@@ -85,11 +85,6 @@ db.saisons.hasMany(db.stands, { foreignKey: "saisonId" });
 db.artistes.belongsToMany(db.sousGenres, { through: "ArtistesSousGenres", foreignKey: "artisteId" });
 db.sousGenres.belongsToMany(db.artistes, { through: "ArtistesSousGenres", foreignKey: "sousGenreId" });
 
-// 1 artiste peut avoir n sous-genres
-// 1 sous-genre peut être lié à n artistes
-db.artistes.belongsToMany(db.genres, { through: "ArtistesGenres", foreignKey: "artisteId" });
-db.genres.belongsToMany(db.artistes, { through: "ArtistesGenres", foreignKey: "GenreId" });
-
 // 1 sousGenre a 1 seul genre
 // 1 genre a plusieurs sousGenres
 db.sousGenres.belongsTo(db.genres, { foreignKey: "genreId" });
